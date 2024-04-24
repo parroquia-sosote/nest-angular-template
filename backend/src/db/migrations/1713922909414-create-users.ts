a698f005bb4a0e7e0c4cb479a388ef53270bbdab
macluiggy <luiggy.macias@imagineapps.co>
2024-04-23 20:42:03 -0500

    Add migration for creating users table

---
+++ backend/src/db/migrations/1713922909414-create-users.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUsers1713922909414 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
