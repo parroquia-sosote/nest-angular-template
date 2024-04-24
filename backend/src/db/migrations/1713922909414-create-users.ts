7f304c5d16bb7b622f139641bad8e0acfd7d6132
macluiggy <luiggy.macias@imagineapps.co>
2024-04-23 20:43:07 -0500

    Create migration for creating users table

--- backend/src/db/migrations/1713922909414-create-users.ts
+++ backend/src/db/migrations/1713922909414-create-users.ts
@@ -1,7 +1,95 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsers1713922909414 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+export class CreateTableUser1684206622652 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'id_number',
+            type: 'varchar',
+            length: '10',
+          },
+          {
+            name: 'full_name',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '100',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '150',
+            isNullable: true,
+          },
+          {
+            name: 'is_password_reset',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'signature',
+            type: 'varchar',
+            length: '255',
+            isNullable: true,
+          },
+          {
+            name: 'is_active',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'role',
+            type: 'varchar',
+            length: '50',
+            default: 'user',
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            length: '20',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp with time zone',
+            isNullable: true,
+          },
+        ],
+        indices: [
+          {
+            name: 'pk_user_id',
+            columnNames: ['id_number'],
+          },
+        ],
+      }),
+      true,
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('auth.user', true);
+  }
 }
