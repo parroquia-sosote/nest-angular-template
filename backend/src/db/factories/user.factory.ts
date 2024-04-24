39f8262c4791d29408f879b874c8425d87511395
macluiggy <luiggy.macias@imagineapps.co>
2024-04-24 18:42:24 -0500

    Update user.factory.ts to use faker.person.fullName() and faker.string.numeric(10)

--- backend/src/db/factories/user.factory.ts
+++ backend/src/db/factories/user.factory.ts
@@ -4,11 +4,11 @@ const { User } = entitiesObject;
 
 export default setSeederFactory(User, (faker) => {
   const user = new User();
-  user.fullName = faker.name.fullName();
+  user.fullName = faker.person.fullName();
   user.email = faker.internet.email(user.fullName);
   user.password = faker.internet.password();
   user.phone = faker.phone.number();
-  user.idNumber = faker.random.numeric(10);
+  user.idNumber = faker.string.numeric(10);
 
   return user;
 });
