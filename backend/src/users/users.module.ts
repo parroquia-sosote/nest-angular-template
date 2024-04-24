852ee94222093e09adf2a872cb8344a83e74dc7e
macluiggy <luiggy.macias@imagineapps.co>
2024-04-23 20:29:09 -0500

    Add UsersController and related files

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users.entitiy';
+import { UsersController } from './users.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], // provides the User entity to the UsersService with the repository injection
   providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
