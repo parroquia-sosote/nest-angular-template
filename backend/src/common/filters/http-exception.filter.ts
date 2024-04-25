183dc8b3256fc9a0a3255aa5b30a85e6c60d32a7
macluiggy <luiggy.macias@imagineapps.co>
2024-04-24 20:38:35 -0500

    Add HttpExceptionFilter to handle HTTP exceptions

---
+++ backend/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ArgumentsHost,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const msg = exception.getResponse
+      ? exception.getResponse()
+      : exception.message;
+    console.error(exception);
+
+    this.logger.error(
+      `Http Status: ${status}, Error Message: ${JSON.stringify(msg)}`,
+    );
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: msg,
+    });
+  }
+}
