b34ec9c8bb271cb1b24e4e81aeea05fe0c307c06
macluiggy <luiggy.macias@imagineapps.co>
2024-04-25 14:38:37 -0500

    Update register component with new form layout and error handling

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  form = {
+    email: '',
+    password: '',
+    username: '',
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.signUp(this.form).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = error.error.message;
+        this.isSignUpFailed = true;
+      },
+    });
+  }
 }
