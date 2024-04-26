7e42887f55768c58359278b6a3bf39bbf40bbead
macluiggy <luiggy.macias@imagineapps.co>
2024-04-26 10:00:15 -0500

    Update app.config.ts to include withFetch() in provideHttpClient()

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,12 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { authInterceptor } from './services/auth/auth.interceptor';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
   ],
 };
