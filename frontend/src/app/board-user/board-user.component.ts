6c00631a284265d3c0e9f02dcf3156c4b6803ee0
macluiggy <luiggy.macias@imagineapps.co>
2024-04-27 11:33:24 -0500

    Update user profile form in board-user component

--- frontend/src/app/board-user/board-user.component.ts
+++ frontend/src/app/board-user/board-user.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './board-user.component.html',
   styleUrl: './board-user.component.scss',
 })
-export class BoardUserComponent {
-  user = {
-    username: 'dummyUser',
-    email: 'dummyEmail@example.com',
-    role: ['user'],
+export class BoardUserComponent implements OnInit {
+  form = {
+    email: '',
+    password: '',
+    username: '',
+    fullName: '',
   };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit() {
+    this.authService.signUp(this.form).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = error.error.message;
+        this.isSignUpFailed = true;
+      },
+    });
+  }
 }
