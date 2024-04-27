74d2b4b2b86ed1fae576e2b627dc55600c9d3b89
macluiggy <luiggy.macias@imagineapps.co>
2024-04-26 22:11:22 -0500

    Update app.component.html and app.component.ts, and home.component.html

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, NavBarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    NavBarComponent,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
