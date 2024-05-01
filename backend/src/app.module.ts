102f2f7958164ff325eed53517bc4c217bc5ecd5
macluiggy <luiggy.macias@imagineapps.co>
2024-04-30 20:31:20 -0500

    Add ServeStaticModule to enable serving static files in the backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,6 +7,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import typeOrmConfig from '../typeorm.config';
 import { SetUserPreferredLanguage } from './lang/middleware/lang.middleware';
 import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+console.log(process.cwd());
 
 @Module({
   imports: [
@@ -15,6 +19,19 @@ import { JwtModule } from '@nestjs/jwt';
     TypeOrmModule.forRoot(typeOrmConfig),
     TypeOrmModule.forFeature([]),
     JwtModule,
+    ServeStaticModule.forRoot({
+      // rootPath: join(__dirname, '..', 'frontend', 'dist', 'frontend'), // adjust this path as needed
+      rootPath: join(
+        __dirname,
+        '..',
+        '..',
+        '..',
+        'frontend',
+        'dist',
+        'frontend',
+        'browser',
+      ), // adjust this path as needed
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
